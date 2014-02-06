f628e5fd05208accaf1f74e92b5afdb9bababdbe
Callum Stott <callum@seadowg.com>
2014-02-06 00:13:42 +0000

    Database YEAH

--- Gemfile
+++ Gemfile
@@ -1,2 +1,7 @@
 source 'https://rubygems.org'
+
 gem 'sinatra'
+
+gem 'sequel'
+gem 'sqlite3'
+gem 'pg'
--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'sequel'
+
+Sequel.connect(ENV["DATABASE_URL"] || "sqlite://tangent.db")
 
 get '/' do
   "We're not going to pay you..."
