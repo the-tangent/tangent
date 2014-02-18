fb6a0f5a986c1dfb7e901302f861caef0f6d59e3
Callum Stott <callum@seadowg.com>
2014-02-18 22:50:06 +0000

    Move template into Nedry

--- app/base.rb
+++ app/base.rb
@@ -5,9 +5,4 @@ class Base < Sinatra::Base
   include Pharrell::Injectable
   injected :article_service, Persistence::ArticleService
   injected :category_service, Persistence::CategoryService
-  
-  def not_authorized
-    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
-    halt 401, erb(:not_authorized)
-  end
 end
--- extensions/sinatra/nedry.rb
+++ extensions/sinatra/nedry.rb
@@ -26,8 +26,26 @@ module Sinatra
       end      
   
       @auth ||= Rack::Auth::Basic::Request.new(request.env)
-      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == editor_credentials
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == admin_credentials
     end
+    
+    def not_authorized
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, NOT_AUTH_TEMPLAT
+    end
+    
+    NOT_AUTH_TEMPLATE = <<-ERB
+      <div style="text-align: center; margin-top: 40px">  
+        <iframe 
+          width="853" 
+          height="480" 
+          src="//www.youtube.com/embed/RfiQYRn7fBg?autoplay=1&loop=1&playlist=RfiQYRn7fBg" 
+          frameborder="0" 
+          allowfullscreen>
+        </iframe>
+        <h1>Ah ah ah!</h1>
+      </div>
+    ERB
   end
 
   helpers Nedry
