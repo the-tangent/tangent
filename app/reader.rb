4d31dff349f067cd1188d8ab80b10917c96b56ab
Callum Stott <callum@seadowg.com>
2014-02-18 19:43:11 +0000

    Add manifesto redirect for production

--- app/reader.rb
+++ app/reader.rb
@@ -1,13 +1,25 @@
 class Reader < Base
   injected :clock, System::Clock
+   
+  get "/" do
+    if ENV["RACK_ENV"] == "production"
+      redirect_to("/manifesto")
+    else
+      redirect to("/home")
+    end
+  end
   
-  get '/' do
+  get "/home" do
     categories = category_service.fetch_all(:articles => article_service)
     date = clock.now
-  
+
     erb :home, :locals => { :categories => categories, :date => date }
   end
-
+  
+  get "/manifesto" do
+    erb :manifesto
+  end
+  
   get "/articles/:id/?" do
     article = article_service.fetch(params[:id])
     widget = Widget::Article.new(article)
