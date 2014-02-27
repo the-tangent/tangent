23c7cca8948884c2146816877f2712ec44634dc8
Callum Stott <callum@seadowg.com>
2014-02-27 22:32:00 +0000

    Use pharrell to create reader db fixtures

--- spec/article_service_spec.rb
+++ spec/article_service_spec.rb
@@ -2,37 +2,29 @@ require_relative "../lib/persistence/database"
 require_relative "../lib/persistence/article_service"
 
 describe Persistence::ArticleService do
-  let(:db) { Persistence::Database.new(ENV["RACK_ENV"], ENV["DATABASE_URL"]).connect }
-  
-  before do
-    RSpec.configure do |config|
-      config.around(:each) do |example|
-        db.transaction(:rollback=>:always){example.run}
-      end
-    end
-  end
-  
+  include Pharrell::Injectable
+
+  injected :service, Persistence::ArticleService
+
   describe "#publish" do
     it "publishes the article the passed id" do
-      service = Persistence::ArticleService.new(db)   
-      ids = service.create("", "", nil, ""), service.create("", "", nil, "") 
-      
+      ids = service.create("", "", nil, ""), service.create("", "", nil, "")
+
       service.publish(ids[0])
-      
+
       expect(service.fetch(ids[0]).published).to be_true
       expect(service.fetch(ids[1]).published).to be_false
     end
   end
-  
+
   describe "#unpublish" do
     it "unpublishes the article the passed id" do
-      service = Persistence::ArticleService.new(db)   
-      ids = service.create("", "", nil, ""), service.create("", "", nil, "") 
+      ids = service.create("", "", nil, ""), service.create("", "", nil, "")
       service.publish(ids[0])
       service.publish(ids[1])
-      
+
       service.unpublish(ids[0])
-      
+
       expect(service.fetch(ids[0]).published).to be_false
       expect(service.fetch(ids[1]).published).to be_true
     end
--- spec/home_page_spec.rb
+++ spec/home_page_spec.rb
@@ -6,16 +6,26 @@ describe "Homepage" do
   include Pharrell::Injectable
 
   injected :article_service, Persistence::ArticleService
+  injected :category_service, Persistence::CategoryService
 
   before do
     Capybara.app = Tangent.new
   end
 
   it "lists articles with summaries" do
-    create_articles(
-      ["Computer Chess", "Roger Ebert", "A good film\n\nAnother thing"],
-      ["Dylan Farrow Is Already Too Old", "Heather Long", "Oh my god Woody Allen\n\nAnother thing"]
-    )
+    article_service.publish(article_service.create(
+      "Roger Ebert",
+      "Computer Chess",
+      nil,
+      "A good film\r\n\r\nAnother thing"
+    ))
+
+    article_service.publish(article_service.create(
+      "Heather Long",
+      "Dylan Farrow Is Already Too Old",
+      nil,
+      "Oh my god Woody Allen\r\n\r\nAnother thing"
+    ))
 
     visit '/'
 
@@ -30,9 +40,12 @@ describe "Homepage" do
 
   describe "clicking on an article" do
     it "shows the article's content" do
-      create_articles(
-        ["Computer Chess", "Roger Ebert", "The *best*:\n\nMovie."],
-      )
+      article_service.publish(article_service.create(
+        "Roger Ebert",
+        "Computer Chess",
+        nil,
+        "The *best*:\n\nMovie."
+      ))
 
       visit '/'
       click_on "Computer Chess"
@@ -44,52 +57,24 @@ describe "Homepage" do
 
   describe "clicking on a category" do
     it "shows articles for that category" do
-      create_categories("Film", "Other")
-      create_articles(
-        ["Other Category Article", "Some Person", "", "Other"],
-        ["Computer Chess", "Roger Ebert", "The *best*:\n\nMovie.", "Film"],
-      )
+      article_service.publish(article_service.create(
+        "Roger Ebert",
+        "Computer Chess",
+        category_service.create("Film"),
+        "some content"
+      ))
+
+      article_service.publish(article_service.create(
+        "Heather Long",
+        "Other Category Article",
+        category_service.create("Other"),
+        "some content"
+      ))
 
       visit '/'
       click_on "Film"
       expect(page).to have_content("Computer Chess")
       expect(page).to have_no_content("Other Category Article")
     end
-  end
-
-  def create_categories(*categories)
-    page.driver.browser.basic_authorize("admin", "admin")
-    visit "/editor"
-
-    categories.each do |category|
-      click_on "Categories"
-      click_on "New Category"
-
-      fill_in "Name", :with => category
-      click_on "Save"
-    end
-
-    page.driver.browser.basic_authorize("wrong", "wrong")
-  end
-
-  def create_articles(*articles)
-    page.driver.browser.basic_authorize("admin", "admin")
-
-    articles.each do |article|
-      visit "/editor"
-      click_on "Articles"
-      click_on "New Article"
-
-      fill_in "Title", :with => article[0]
-      fill_in "Author", :with => article[1]
-      fill_in "Content", :with => article[2]
-      select article[3], :from => "Category" if article[3]
-
-      click_on "Save"
-      click_on article[0]
-      click_on "Publish"
-    end
-
-    page.driver.browser.basic_authorize("wrong", "wrong")
   end
 end
