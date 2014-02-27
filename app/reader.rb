33c624d41868ab73db73121ad84d6d6891ab7ea6
Callum Stott <callum@seadowg.com>
2014-02-27 20:31:13 +0000

    Use nav bar on article show

--- app/reader.rb
+++ app/reader.rb
@@ -1,6 +1,6 @@
 class Reader < Base
   helpers Sinatra::Nedry
-   
+
   get "/" do
     if ENV["RACK_ENV"] == "production"
       redirect to("/manifesto")
@@ -8,39 +8,42 @@ class Reader < Base
       redirect to("/home")
     end
   end
-  
+
   get "/manifesto" do
     erb :manifesto
   end
-  
+
   get "/home" do
     flagged! do
       categories = category_service.fetch_all
-      
+
       articles = article_service.fetch_all.select { |a| a.published }
       tiles = articles.map { |a| Widget::Tile.new(a) }
-      
-      erb :home, :locals => { :categories => categories, :articles => tiles }
+
+      erb :articles, :locals => { :categories => categories, :articles => tiles }
     end
   end
-  
+
   get "/categories/:id/?" do
     flagged! do
       categories = category_service.fetch_all
-      
-      articles = article_service.fetch_all_from_category(params[:id]).select { |a| a.published }
-      tiles = articles.map { |a| Widget::Tile.new(a) }
-      
-      erb :home, :locals => { :categories => categories, :articles => tiles }
+
+      articles = article_service.fetch_all_from_category(params[:id])
+      published_articles = articles.select { |a| a.published }
+      tiles = published_articles.map { |a| Widget::Tile.new(a) }
+
+      erb :articles, :locals => { :categories => categories, :articles => tiles }
     end
   end
-  
+
   get "/articles/:id/?" do
     flagged! do
+      categories = category_service.fetch_all
+
       article = article_service.fetch(params[:id])
       widget = Widget::Article.new(article)
-  
-      erb :articles_show, :locals => { :article => widget }
+
+      erb :articles_show, :locals => { :article => widget, :categories => categories }
     end
   end
 end
