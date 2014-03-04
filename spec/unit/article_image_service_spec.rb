6c6a2666f1567919ac46be2c96f91672f30db2f6
Callum Stott <callum@seadowg.com>
2014-03-04 00:04:30 +0000

    Use fake fog for article image service spec

--- spec/unit/article_image_service_spec.rb
+++ spec/unit/article_image_service_spec.rb
@@ -2,25 +2,11 @@ require_relative "../../lib/persistence/database"
 require_relative "../../lib/persistence/article_service"
 
 describe Persistence::ArticleService do
-  let(:storage) {
-    Fog.mock!
-
-    fog = Fog::Storage.new(
-      :provider => "AWS",
-      :aws_access_key_id => "",
-      :aws_secret_access_key => ""
-    )
-
-    FOG.directories.create(
-      :key    => "test",
-      :public => true
-    )
-  }
-
+  let(:storage) { TestFogBucket.new }
   let(:service) { Persistence::ArticleImageService.new(storage) }
 
   describe "#upload" do
-    it "uploads the file" do
+    it "uploads the file publically" do
       file = { :filename => "test.png", :tempfile => Tempfile.new("test.png") }
       id = "1"
 
@@ -28,6 +14,33 @@ describe Persistence::ArticleService do
 
       expect(storage.files.length).to eq(1)
       expect(storage.files.first.key).to eq("article1-test.png")
+      expect(storage.files.first.body).to eq(file[:tempfile])
+      expect(storage.files.first.public).to eq(true)
+    end
+  end
+
+  private
+
+  class TestFogBucket
+    def initialize
+      @files = []
+    end
+
+    def files
+      self
+    end
+
+    def create(file)
+      @files << OpenStruct.new(file)
+      OpenStruct.new(:public_url => "")
+    end
+
+    def length
+      @files.length
+    end
+
+    def first
+      @files.first
     end
   end
 end
