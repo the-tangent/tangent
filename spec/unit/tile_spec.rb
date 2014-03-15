843dd188bf261dbe63979ee43434af620a04d93f
Callum Stott <callum@seadowg.com>
2014-03-15 16:32:28 +0000

    Parse windows newlines correctly for article summary

---
+++ spec/unit/tile_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe Widget::Tile do
+  describe "#summary" do
+    it "returns the first paragraph for unix new lines" do
+      tile = Widget::Tile.new(Persistence::Model.new(
+        :id => 1,
+        :title => "Title",
+        :content => "First para\n\nSecond para"
+      ))
+
+      expect(tile.summary).to include("First para")
+      expect(tile.summary).to_not include("Second para")
+    end
+
+    it "returns the first paragraph for windows new lines" do
+      tile = Widget::Tile.new(Persistence::Model.new(
+        :id => 1,
+        :title => "Title",
+        :content => "First para\r\n\r\nSecond para"
+      ))
+
+      expect(tile.summary).to include("First para")
+      expect(tile.summary).to_not include("Second para")
+    end
+  end
+end
