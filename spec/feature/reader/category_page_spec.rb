19e842a28cb8b713bf869ce597bc28b5ec7b2388
Callum Stott <callum@seadowg.com>
2014-03-15 23:39:40 +0000

    Fix home page link

---
+++ spec/feature/reader/category_page_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+describe "Homepage" do
+  include Capybara::DSL
+  include Pharrell::Injectable
+
+  injected :article_service, Persistence::ArticleService
+  injected :clock, System::Clock
+
+  before do
+    Capybara.app = Tangent.new
+  end
+
+  it "links to the home page" do
+    article_service.publish(article_service.create(
+      "Roger Ebert",
+      "Computer Chess",
+      Categories::FILM.id,
+      "some content"
+    ), Time.at(0))
+
+    article_service.publish(article_service.create(
+      "Heather Long",
+      "Other Category Article",
+      Categories::LIFE.id,
+      "some content"
+    ), Time.at(0))
+
+    visit '/'
+    click_on "Film"
+
+    click_on "Home"
+    expect(page).to have_content("Computer Chess")
+    expect(page).to have_content("Other Category Article")
+  end
+end
